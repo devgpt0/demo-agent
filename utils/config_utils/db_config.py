193b8f377c12f8d80f056ebaae2597a85f78dce5
AdarshRai3 <adarshaibusiness@gmail.com>
2025-08-24 22:28:10 +0530

    Add:Taking first_name of candidate

--- utils/config_utils/db_config.py
+++ utils/config_utils/db_config.py
@@ -3,9 +3,12 @@ from upstash_redis import Redis
 import os
 from dotenv import load_dotenv
 from utils.config_utils.config_loader import get_config 
+from utils.monitoring_utils.logging import get_logger
 
+logger = get_logger("db-config")
 # Create Redis client using REST credentials
 redis = Redis(
     url=get_config("UPSTASH_REDIS_URL"),
     token=get_config("UPSTASH_REDIS_TOKEN")
 )
+
